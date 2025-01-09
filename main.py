bf158069d0a8cfe840f8d75b8854f665fe0da2e4
Turaeva Viktoriya <vika.tureava.gogol@gmail.com>
2025-01-09 20:14:06 +0500

    add more func for course service

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.utils.course_service import add_course
 from src.modules.student import Student
 from src.utils.profile_service import view_student_profile
+from src.utils.course_service import add_course, edit_course, delete_course
 
 # List to store all courses
 course_list = []
@@ -9,7 +10,13 @@ course_list = []
 add_course(course_list, course_id=101, name="Math 101", capacity=30)
 add_course(course_list, course_id=102, name="Science 102", capacity=25)
 
-# Display all courses
+# Editing a course
+edit_course(course_list, course_id=101, new_name="Advanced Math", new_capacity=40)
+
+# Deleting a course
+delete_course(course_list, course_id=102)
+
+# Display all remaining courses
 print("\nAvailable Courses:")
 for course in course_list:
     print(course)
